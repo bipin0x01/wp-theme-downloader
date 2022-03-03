2561fd3c9aa1acf4b954e7022dbfb8d4f4564c52
Bipin Thapa <bt.kaji@gmail.com>
2022-03-04 01:37:51 +0545

    feat:generate theme download link

--- src/theme-downloader/theme-downloader.controller.ts
+++ src/theme-downloader/theme-downloader.controller.ts
@@ -20,7 +20,38 @@ export class ThemeDownloaderController {
       const site = await axios.get(siteLink);
       const $ = cheerio.load(site.data);
       const html = $.root().html();
-      return html;
+      let links = [];
+      $('link').each((i, link) => {
+        links.push($(link).attr('href'));
+      });
+
+      //   strip the input url of the domain name and only get the name
+      const name = siteLink.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '');
+
+      // regex pattern to match the style.css file
+      const regex = new RegExp(
+        `${name}\/wp-content\/([a-zA-Z]+(\/[a-zA-Z]+)+)\/style.css`,
+        'g',
+      );
+
+      //   check if the styles file exists in the links array
+      const styleFile = links.find((link) => regex.test(link));
+      if (styleFile) {
+        //   if the regex file exists, return the element that matches the regex
+        const themePath = styleFile.split('/');
+        const themeName = themePath[themePath.length - 2] + '.zip';
+        const themeUrl = `${siteLink}/wp-content/themes/${themeName}`;
+
+        const checkTheme = await axios.get(themeUrl);
+        if (checkTheme.status !== 404) {
+          return themeUrl;
+        } else {
+          return "Theme not found! Seems like There's nothing left for you.";
+        }
+      } else {
+        //   if it doesn't, return the error message
+        return 'It is not an wordpress website';
+      }
     } catch (error) {
       return error;
     }
