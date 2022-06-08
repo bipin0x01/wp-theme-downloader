82f2358220763d082614c27001366234be6686a8
Bipin Thapa <bipintube552@gmail.com>
2022-06-09 00:00:23 +0545

    fix: import HttpCode decorator

--- src/theme-downloader/theme-downloader.controller.ts
+++ src/theme-downloader/theme-downloader.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Render, Header } from '@nestjs/common';
+import { Controller, Post, Body, Get, Render, Header, HttpCode } from '@nestjs/common';
 import { ThemeDownloaderService } from './theme-downloader.service';
 
 // types
